4f8a5f6f02d886223d6981be3fe65e37136bfa0a
orygynyl <ebenezerowereko111@gmail.com>
2026-03-15 21:30:24 +0000

    nterpolation

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,4 +1,13 @@
 void main() {
  String schoolName = 'academy';
  print(schoolName);
+
+ // nterpolation
+ String farmerName = 'Ama Owusu';
+int yearsOfExperience = 7;
+double fieldSizeInAcres = 3.5;
+print('Farmer: $farmerName');
+print('Experience: $yearsOfExperience years');
+print('Field size: ${fieldSizeInAcres} acres');
+print('$farmerName has farmed for $yearsOfExperience years.');
 }
