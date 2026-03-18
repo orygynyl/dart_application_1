9ff4e0d8e704710f92fb1ff9e37e066a5b9ee5d4
orygynyl <ebenezerowereko111@gmail.com>
2026-03-18 17:52:57 +0000

    switch days of the week

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -8,6 +8,53 @@ int yearsOfExperience = 7;
 double fieldSizeInAcres = 3.5;
 print('Farmer: $farmerName');
 print('Experience: $yearsOfExperience years');
-print('Field size: ${fieldSizeInAcres} acres');
+print('Field size: ${fieldSizeInAcres * yearsOfExperience} acres');
 print('$farmerName has farmed for $yearsOfExperience years.');
+
+print('Farm range: ${fieldSizeInAcres + yearsOfExperience} years');
+
+// switch season
+String season = 'harmattan';
+switch (season) {
+case 'rainy':
+print('Plant maize, yam, and cassava.');
+print('Reduce irrigation — rain will help!');
+break;
+case 'wet':
+print('Plant drought-resistant crops like sorghum.');
+print('Increase irrigation frequency.');
+break;
+case 'harmattan':
+print('Protect crops from dry winds.');
+print('Mulch the soil to retain moisture.');
+break;
+default:
+print('Unknown season. Please check your input.');
 }
+
+
+//switch days of the week
+String daysOfTheWeek = 'friday';
+switch (daysOfTheWeek) {
+case 'monday':
+    print('good morning');
+    print('how are you doing');
+    break;
+case 'tuesday':
+    print('good afternoon');
+    print('hope you are fine?');
+    break;
+case 'wednesday':
+    print('thursday');
+    print('how are you doing');
+    break;
+case 'saturday':
+    print('good evening everyone');
+    print('how was your day?');
+    break;
+default:
+    print('Unknown day, check your input');
+}
+ 
+}
+
